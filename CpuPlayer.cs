d9aa9c4be32956ea4eca4b14adafdde7e668c691
ZoutigeWolf <guuskamphuis@gmail.com>
2022-04-06 21:01:28 +0200

    Bruh

--- CpuPlayer.cs
+++ CpuPlayer.cs
@@ -20,5 +20,31 @@ namespace Yahtzee
         {
             _dice = new Dice(5);
         }
+
+        private void RollDice()
+        {
+            _dice.Roll();
+
+            _rollsRemaining--;
+
+            KeyValuePair<string, float>[] chances = GetChances();
+        }
+
+        private KeyValuePair<string, float>[] GetChances()
+        {
+            List<KeyValuePair<string, float>> chances = new List<KeyValuePair<string, float>>();
+
+            chances.Add(new KeyValuePair<string, float>("Ones", _dice.Values.Where(x => x == 1).Count() / 3 * 100f));
+            chances.Add(new KeyValuePair<string, float>("Twos", _dice.Values.Where(x => x == 2).Count() / 3 * 100f));
+            chances.Add(new KeyValuePair<string, float>("Threes", _dice.Values.Where(x => x == 3).Count() / 3 * 100f));
+            chances.Add(new KeyValuePair<string, float>("Fours", _dice.Values.Where(x => x == 4).Count() / 3 * 100f));
+            chances.Add(new KeyValuePair<string, float>("Fives", _dice.Values.Where(x => x == 5).Count() / 3 * 100f));
+            chances.Add(new KeyValuePair<string, float>("Sixes", _dice.Values.Where(x => x == 6).Count() / 3 * 100f));
+
+            chances.Add(new KeyValuePair<string, float>("Three of a kind", _dice.Values.ToArray().HighestValueCount() / 3 * 100f));
+            chances.Add(new KeyValuePair<string, float>("Four of a kind", _dice.Values.ToArray().HighestValueCount() / 4 * 100f));
+
+            return chances.ToArray();
+        }
     }
 }
--- Extensions.cs
+++ Extensions.cs
@@ -12,6 +12,21 @@ namespace Yahtzee
         {
             return array.GroupBy(i => i).Any(g => g.Count() == n);
         }
+
+        public static int HighestValueCount(this int[] array)
+        {
+            int highest = 0;
+
+            for (int i = 1; i <= 6; i++)
+            {
+                int count = array.Count(x => x == i);
+
+                highest = count > highest ? count : highest;
+            }
+
+            return highest;
+        }
+
         public static bool IsIncreasing(this int[] array, int n)
         {
             for (int i = 0; i < array.Length - n + 1; i++)
